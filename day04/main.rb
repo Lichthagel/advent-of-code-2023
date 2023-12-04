531e53057c6fa05f582dc28decc0d5f8b25e2e2f
Lichthagel <hey@lichthagel.de>
2023-12-04 10:20:06 +0100

    day04: cleanup

--- day04/main.rb
+++ day04/main.rb
@@ -1,39 +1,23 @@
 #!/usr/bin/env ruby
 
+require 'set'
+
 if ARGV.length != 1
   raise "Please specify an input file"
 end
 
 input = File.read(ARGV[0]).split("\n")
 
-sum = 0
+cards = input.map { |line| line.split(": ")[1].split(" | ").map { |card|  card.split(" ").map(&:to_i).to_set } }
+counts = cards.map { |card| (card[0] & card[1]).length }
 
-cards = input.map { |line| line.split(": ")[1].split(" | ").map { |card| card.split(" ").map(&:to_i) } }
-
-def card_count(card)
-  count = 0
-  for num in card[1]
-    count += 1 if card[0].include?(num)
-  end
-
-  count
-end
-
-for card in cards
-  count = card_count(card)
-
-  if count > 0
-    sum += 2 ** (count - 1)
-  end
-end
-
-puts sum
+puts counts.map { |count| count > 0 ? 2 ** (count - 1) : 0 }.sum
 
 my_cards = [1] * cards.length
 
 for i in 0..cards.length - 1
   card = cards[i]
-  count = card_count(card)
+  count = counts[i]
 
   if count > 0
     for j in 1..count
