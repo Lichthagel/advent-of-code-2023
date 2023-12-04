aaf60c43deb234f5bb188895cd9c385fc15321ee
Lichthagel <hey@lichthagel.de>
2023-12-04 11:06:12 +0100

    day04 golf: use count instead of length

--- day04/golf.rb
+++ day04/golf.rb
@@ -5,9 +5,9 @@ s = File.read("input.txt").split("\n")
 x = 0
 m = s.map { 1 }
 
-for i in 0..s.length-1
+for i in 0..s.count-1
   a, b = s[i].split(": ")[1].split(" | ").map { |e| e.split().map(&:to_i).to_set }
-  c = (a & b).length
+  c = (a & b).count
   x += c > 0 ? 2 ** (c - 1) : 0
 
   for j in 1..c
