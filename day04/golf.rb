2e0d35e62ffeeb4aef0656e63287b0e94c8f1313
Lichthagel <hey@lichthagel.de>
2023-12-04 10:48:25 +0100

    day04: improve golf

--- day04/golf.rb
+++ day04/golf.rb
@@ -1,15 +1,19 @@
 require 'set'
 
-s = File.read("input.txt").split("\n").map { |l| l.split(": ")[1].split(" | ").map { |e|  e.split(" ").map(&:to_i).to_set } }.map { |e| (e[0] & e[1]).length }
+s = File.read("input.txt").split("\n")
 
-puts s.map { |c| c > 0 ? 2 ** (c - 1) : 0 }.sum
-
+x = 0
 m = [1] * s.length
 
 for i in (0..(s.length-1))
-  for j in (1..s[i])
+  e = s[i].split(": ")[1].split(" | ").map { |e| e.split(" ").map(&:to_i).to_set }
+  c = (e[0] & e[1]).length
+  x += c > 0 ? 2 ** (c - 1) : 0
+
+  for j in (1..c)
     m[i + j] += m[i]
   end
+  
 end
 
-puts m.sum
+puts x, m.sum
