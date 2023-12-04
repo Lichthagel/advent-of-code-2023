83f281fd54db6ad45cb8306a335370ee5cbee3e5
Lichthagel <hey@lichthagel.de>
2023-12-04 10:36:23 +0100

    day04: actual good golf

--- day04/golf.rb
+++ day04/golf.rb
@@ -1,8 +1,8 @@
 require 'set'
 
-s = File.read("input.txt").split("\n").map { |l| l.split(": ")[1].split(" | ").map { |e|  e.split(" ").map(&:to_i).to_set } }.map { |card| (card[0] & card[1]).length }
+s = File.read("input.txt").split("\n").map { |l| l.split(": ")[1].split(" | ").map { |e|  e.split(" ").map(&:to_i).to_set } }.map { |e| (e[0] & e[1]).length }
 
-puts s.map { |count| count > 0 ? 2 ** (count - 1) : 0 }.sum
+puts s.map { |c| c > 0 ? 2 ** (c - 1) : 0 }.sum
 
 m = [1] * s.length
 
