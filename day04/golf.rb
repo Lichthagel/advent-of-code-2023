9ef1b0f94fbe3785b0f0dd1a423661ac9ece9c91
Lichthagel <hey@lichthagel.de>
2023-12-04 10:53:38 +0100

    day04: golf remove braces

--- day04/golf.rb
+++ day04/golf.rb
@@ -5,12 +5,12 @@ s = File.read("input.txt").split("\n")
 x = 0
 m = [1] * s.length
 
-for i in (0..(s.length-1))
+for i in 0..s.length-1
   e = s[i].split(": ")[1].split(" | ").map { |e| e.split(" ").map(&:to_i).to_set }
   c = (e[0] & e[1]).length
   x += c > 0 ? 2 ** (c - 1) : 0
 
-  for j in (1..c)
+  for j in 1..c
     m[i + j] += m[i]
   end
   
